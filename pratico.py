2c380c086b57aac972aad7524f75a9fadcd6d6dd
Pereira3 <ricardojapereira@gmail.com>
2024-05-13 14:14:04 +0100

    Assinatura e Verificação, Help e ERROS (Ver TODO.txt)

--- pratico.py
+++ pratico.py
@@ -7,6 +7,13 @@ import time
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+# IMPORTS ASSINATURA DIGITAL
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives.asymmetric import rsa
+# Preciso meter uma variável para evitar conflitos com o padding importado em cima
+from cryptography.hazmat.primitives.asymmetric import padding as as_padding
+
 
 
 #-----------------------------------------------ENCRYPTAR-----------------------------------------------
@@ -19,6 +26,11 @@ def encrypt(input_file, output_file, key):
     # Obrigatório ser do tipo global para poder verificar o valor da mesma no Decrypt
     global hmac_hashFO 
     hmac_hashFO = calc_hash_hmac(input_file, key)
+    
+    # Gera as Chaves Privada e Pública, devolve a Chave Privada e a Assinatura
+    global file_sign
+    # Na criação de variáveis em separado, corria a função duas vezes, gerava duas assinaturas diferentes
+    file_sign = dig_sig(input_file)
 
     # Gera o pin random de 3 a 4 digitos
     pin = str(random.randint(100,9999))
@@ -105,7 +117,7 @@ def decrypt(input_file, output_file):
     # Enquanto tiver tentativas restantes continua o programa
     while(contador > 0):
 
-        pin = input("Decrypt PIN ("+str(contador)+" Tentativas): ")
+        pin = input("\nDecrypt PIN ("+str(contador)+" Tentativas): ")
 
         # Fazer a mesma coisa com o pin la em cima, se o pin for o mesmo vai decifrar bem o ficheiro 
         # Se nao vai dar erro e o try/catch vai apanhar diminuindo o numero de tentativas do pin
@@ -176,9 +188,16 @@ def decrypt(input_file, output_file):
     if(contador == 0): # 0 tentativas -> Apaga o ficheiro
         print("\nEsgotou Tentativas -> Ficheiro Apagado\n")
     else:
+
         # Calcula o HMAC E HASH VALUE DO FICHEIRO DECIFRADO
         hmac_hashFD = calc_hash_hmac(output_file, key)
-        
+
+        # Verificar a Assinatura do ficheiro de Output/Decifrado
+        if( ver_sig(output_file, file_sign[0], file_sign[1]) ):
+            print("\nFicheiro com Assinatura Válida")
+        else:
+            print("\nFicheiro com Assinatura Inválida")
+
         if hmac_hashFD == hmac_hashFO: # Se o valor hash do message authenticator do ficheiro original for igual do ficheiro decifrado, os ficheiros não sofreram alteração
             print("Ficheiro Decifrado -> Diretoria Decifrado")
             # Apagar o chaves.bin do ficheiro correspondente
@@ -213,41 +232,109 @@ def calc_hash_hmac(file_path, key):
 
 
 
+#-----------------------------------------------Dig. Signature-----------------------------------------------
+def dig_sig(input_file):
+
+    # Para poder verificar a assinatura com a assinutura resultante desta função
+    global signature
+
+    # Gera uma chave privada com public_exponent e tamanho recomendados pela documentação
+    priv_k = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    with open(input_file, 'rb') as f:
+        plaintext = f.read()
+
+    signature = priv_k.sign(
+        plaintext,
+        # PSS -> Padding recomendado
+        as_padding.PSS(
+            # Mask Generation Function -> Gera uma máscara quando é dado o padding ao ficheiro usando a função SHA256
+            mgf = as_padding.MGF1(hashes.SHA256()),
+            # Indica o tamanha máxima permitido no padding -> Neste caso é o máximo que o PSS permite
+            salt_length = as_padding.PSS.MAX_LENGTH
+        ),
+        # Algoritmo e valor de hash resultado do uso do algoritmo para a encriptação necessária para a produção da Assinatura
+        hashes.SHA256()
+    )
+
+    return [priv_k, signature]
+
+
+
+
+
+#-----------------------------------------------Verificar Signature-----------------------------------------------
+def ver_sig(input_file, priv_k, signature):
+
+    # Gera uma chave pública a partir da chave privada gerada anteriormente
+    pk = priv_k.public_key()
+    
+    with open(input_file, 'rb') as f:
+        plaintext = f.read()
+
+    try:
+        pk.verify(
+            signature,
+            plaintext,
+            as_padding.PSS(
+                # Mask Generation Function -> Gera uma máscara quando é dado o padding ao ficheiro usando a função SHA256
+                mgf = as_padding.MGF1(hashes.SHA256()),
+                # Indica o tamanha máxima permitido no padding -> Neste caso é o máximo que o PSS permite
+                salt_length = as_padding.PSS.MAX_LENGTH
+            ),
+            # Algoritmo e valor de hash resultado do uso do algoritmo para a encriptação necessária para a produção da Assinatura anteriormente
+            hashes.SHA256()
+        )
+        # Verificou e as assinaturas são iguais
+        return True
+    except: # Se a assinatura não corresponder
+        return False
+
+
+
 #-----------------------------------------------TESTES/MAIN-----------------------------------------------
 key = os.urandom(32) # A key ta aqui para testes, no final vai ser criada no mesmo sitio que o iv
 
+e = input("\n-----Caso queira recorrer ao manual carregue em H/h-----\n-----Caso queira prosseguir carregue em P/p-----\n\nEscolha: ")
+escolha = e.upper()
+
 # PARA TESTAR: Criar um ficheiro (de preferência fora da FALL...), mover o ficheiro para a FALL-INTO-OBLIVION, mover de seguida para o Recuperacao e meter o PIN
 while(True):
-     
-    e = os.listdir("./FALL-INTO-OBLIVION")
-    d = os.listdir("./Recuperacao")
+    match escolha:
+        case "H":
+            print("\n-----Help----\nO Programa consiste numa Reciclagem de um sistema operativo normal alterada, ao eliminar os ficheiros (Move-los para a pasta FALL-INTO-OBLIVION) é lhe dado um PIN correspondente ao ficheiro que enviou.\nPara recuperar o ficheiro terá de movê-lo outra vez para a pasta Recuperação e terá três tentativas para inserir esse código PIN.\nCaso não acerte o PIN nas três tentativas dadas o ficheiro é eliminado permanentemente do seu sistema, caso acerte é verificada a integridade do ficheiro de modo a saber se foi alterado ou corrompido no processo todo.\nPor fim, caso tenha acertado o PIN terá o ficheiro recuperado na pasta Decifrados.\nNo processo de recuperação é lhe informada a integridade.")
+            # CANCELAR O LOOP
+            escolha = "C"
+        case "P":
+            e = os.listdir("./FALL-INTO-OBLIVION")
+            d = os.listdir("./Recuperacao")
 
-    # Se a diretoria FALL-INTO-OBLIVION tiver algum ficheiro com a extensão de texto (txt), irá invocar a função de encriptação
-    if len(e) > 0:
-        for i in e:
-            if i.endswith('.txt'):
-                # Invoca a função encrypt com o ficheiro como input e devolve um ficheiro cifrado com a cifra AES256-cbc
-                encrypt("./FALL-INTO-OBLIVION/"+i, "./FALL-INTO-OBLIVION/"+os.path.splitext(i)[0]+".aes256-cbc", key)
-                # Após cifrar o ficheiro, remove o ficheiro original da pasta FALL-INTO-OBLIVION (Plaintext)
-                os.remove("./FALL-INTO-OBLIVION/"+i)
+            # Se a diretoria FALL-INTO-OBLIVION tiver algum ficheiro com a extensão de texto (txt), irá invocar a função de encriptação
+            if len(e) > 0:
+                for i in e:
+                    if i.endswith('.txt'):
+                        # Invoca a função encrypt com o ficheiro como input e devolve um ficheiro cifrado com a cifra AES256-cbc
+                        encrypt("./FALL-INTO-OBLIVION/"+i, "./FALL-INTO-OBLIVION/"+os.path.splitext(i)[0]+".aes256-cbc", key)
+                        # Após cifrar o ficheiro, remove o ficheiro original da pasta FALL-INTO-OBLIVION (Plaintext)
+                        os.remove("./FALL-INTO-OBLIVION/"+i)
 
-    # Se a diretoria Recuperacao tiver algum ficheiro com a extensão de texto (aes256-cbc), irá invocar a função de desencriptação
-    if len(d) > 0:
-        for i in d:
-            if i.endswith('.aes256-cbc'):
-                # Invoca a função decrypt com o ficheiro cifrado como input e devolve um ficheiro decifrado para a pasta de Decifrados após a inserção do pin correto
-                decrypt("./Recuperacao/"+os.path.splitext(i)[0]+".aes256-cbc", "./Decifrados/"+os.path.splitext(i)[0]+".txt")
-                # Após decifrar o ficheiro, remove o criptograma da pasta de Recuperacao
-                os.remove("./Recuperacao/"+os.path.splitext(i)[0]+".aes256-cbc")
-    
-    #Verifica as pastas a cada segundo
-    time.sleep(1)
-
-
-
-
-
-
+            # Se a diretoria Recuperacao tiver algum ficheiro com a extensão de texto (aes256-cbc), irá invocar a função de desencriptação
+            if len(d) > 0:
+                for i in d:
+                    if i.endswith('.aes256-cbc'):
+                        # Invoca a função decrypt com o ficheiro cifrado como input e devolve um ficheiro decifrado para a pasta de Decifrados após a inserção do pin correto
+                        decrypt("./Recuperacao/"+os.path.splitext(i)[0]+".aes256-cbc", "./Decifrados/"+os.path.splitext(i)[0]+".txt")
+                        # Após decifrar o ficheiro, remove o criptograma da pasta de Recuperacao
+                        os.remove("./Recuperacao/"+os.path.splitext(i)[0]+".aes256-cbc")
+            
+            #Verifica as pastas a cada segundo
+            time.sleep(1)
+        case _:
+            e = input("\n-----Caso queira recorrer ao manual carregue em H/h-----\n-----Caso queira prosseguir carregue em P/p-----\n\nEscolha: ")
+            escolha = e.upper()
 
 
     '''option = input("\nE/e -> Encrypt\nD/d -> Decrypt\nChosen Option: ")
